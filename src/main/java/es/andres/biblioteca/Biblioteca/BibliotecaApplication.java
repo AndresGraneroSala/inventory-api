ace140176a988977c68ded332218e7ee48944f45
AndresGS <andresnn21@gmail.com>
2026-07-02 06:51:57 +0200

    add environment variables

--- src/main/java/es/andres/biblioteca/Biblioteca/BibliotecaApplication.java
+++ src/main/java/es/andres/biblioteca/Biblioteca/BibliotecaApplication.java
@@ -7,19 +7,15 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BibliotecaApplication {
-
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(BibliotecaApplication.class, args);
 		Environment env = context.getEnvironment();
 
 		String port = env.getProperty("server.port");
 		String apiDocsPath = env.getProperty("springdoc.api-docs.path");
-
 		String swaggerPath = env.getProperty("springdoc.swagger-ui.path");
 
 		System.out.println("API-DOCS: http://localhost:" + port + apiDocsPath);
 		System.out.println("SWAGGER: http://localhost:" + port + swaggerPath);
 	}
 }
-
-//4:15
